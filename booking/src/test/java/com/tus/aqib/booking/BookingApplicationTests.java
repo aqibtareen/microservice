2d1f07367ad1c552be00e3d53e786f5099111d34
Aqib Tareen <aqibtareen7@gmail.com>
2024-11-21 22:01:47 +0000

    Initialization of spring boot

---
+++ booking/src/test/java/com/tus/aqib/booking/BookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tus.aqib.booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
